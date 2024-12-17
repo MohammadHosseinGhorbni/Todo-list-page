27e3a0edf34b8330e6f3aae72568b80236e126da
M.Hossein <hoseing211@gmail.com>
2024-12-17 12:24:03 +0330

    Add files via upload

---
+++ todo-list-page/source/js/script.js
@@ -0,0 +1,37 @@
+﻿let $ = document;
+let inputElem = $.querySelector("#form-input");
+let addInputElem = $.querySelector(".add");
+let formUlElem = $.querySelector("#ul-elem");
+let trashElem = $.querySelectorAll(".delete");
+
+function addNewTodo(addNewValue) {
+  let addNewLi = $.createElement("li");
+  addNewLi.className =
+    "list-group-item d-flex justify-content-between align-items-center";
+  let addNewSpan = $.createElement("span");
+  addNewSpan.innerHTML = addNewValue;
+  let addNewTrash = $.createElement("i");
+  addNewTrash.className = "fa fa-trash-o delete";
+
+  addNewTrash.addEventListener("click", function (event) {
+    event.target.parentElement.remove();
+  });
+
+  addNewLi.append(addNewSpan, addNewTrash);
+  formUlElem.append(addNewLi);
+}
+
+addInputElem.addEventListener("submit", function (event) {
+  event.preventDefault();
+});
+
+inputElem.addEventListener("keydown", function (event) {
+  let addNewValue = event.target.value.trim();
+
+  if (event.keyCode === 13) {
+    if (addNewValue) {
+      inputElem.value = "";
+      addNewTodo(addNewValue);
+    }
+  }
+});
